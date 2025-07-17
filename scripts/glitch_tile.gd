f871c89cac306f1e4bd6da0592f44bcf20a8018b
AIChan <201923244+Serial-Cyn@users.noreply.github.com>
2025-07-17 20:31:29 +0800

    Improved Level 1

--- scripts/glitch_tile.gd
+++ scripts/glitch_tile.gd
@@ -2,9 +2,14 @@ extends Node2D
 
 @onready var sprite: Sprite2D = $Sprite
 @onready var collider: CollisionShape2D = $StaticBody/Collider
+@onready var timer: Timer = $Timer
 
+var is_visible: bool = true
 
-var is_visible : bool = true
+func _ready():
+	# Set random wait time between 0.5 to 2 seconds (you can change the range)
+	timer.wait_time = randf_range(0.5, 2.0)
+	timer.start()
 
 func _on_timer_timeout() -> void:
 	is_visible = !is_visible
--- scripts/patch.gd
+++ scripts/patch.gd
@@ -1,13 +1,37 @@
 extends CharacterBody2D
 
+@onready var next_level_timer: Timer = $NextLevelTimer
+@onready var player: CharacterBody2D = %Player
 
 const FILE_BEGIN = "res://levels/level"
 const FILE_END = ".tscn"
 
+var current_scene_file
+var current_level
+
+func _ready() -> void:
+	current_scene_file = get_tree().current_scene.scene_file_path
+	current_level = current_scene_file.to_int()
+	
+	
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
-		var current_scene_file = get_tree().current_scene.scene_file_path
-		var next_level_number = current_scene_file.to_int() + 1
+		match current_level:
+			1:
+				player.gravity_direction = 1
+				player.reverse_control = 1
+				
+			2:
+				pass
+		# next_level_timer.start()
+
+
+func next_level() -> void:
+	var next_level_number = current_level + 1
 		
-		var next_level_path = FILE_BEGIN + str(next_level_number) + FILE_END
-		get_tree().change_scene_to_file(next_level_path)
+	var next_level_path = FILE_BEGIN + str(next_level_number) + FILE_END
+	get_tree().change_scene_to_file(next_level_path)
+
+
+func _on_next_level_timer_timeout() -> void:
+	next_level()
